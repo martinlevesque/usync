cf6021c5fe5ae6723fc0a03958bde27b1a1a8e52
Martin Levesque <levesque.martin@gmail.com>
2022-05-23 17:15:20 -0400

    wip

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/martinlevesque/usync
+
+go 1.18
+
+replace github.com/martinlevesque/usync/io/in/test ./io/in/test
---
+++ main.go
@@ -0,0 +1,15 @@
+
+package main
+  
+import (
+	"fmt"
+	"github.com/martinlevesque/usync/io/in/test"
+)
+  
+// Main function
+func main() {
+
+	test.hello()
+  
+    fmt.Println("!... Hello World ...!")
+}
